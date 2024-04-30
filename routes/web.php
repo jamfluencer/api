128e600814061922521ce7ba4b63701533792a4d
Marshall Davis <toothlessrebel@gmail.com>
2024-04-30 08:16:14 -0400

    Add redirect option

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,35 @@ Route::get('/', fn () => response()->json(['status' => 'OK']));
 
 Route::get(
     '/auth/google',
-    fn (): JsonResponse => response()
-        ->json(['url'=>Socialite::driver('google')->stateless()->redirect()->getTargetUrl()])
+    function (Request $request): JsonResponse {
+        $request->validate([
+            'redirect' => ['required', 'url'],
+        ]);
+
+        return response()
+            ->json([
+                'url' => Socialite::driver('google')
+                    ->stateless()
+                    ->redirectUrl($request->input('redirect'))
+                    ->redirect()
+                    ->getTargetUrl(),
+            ]);
+    }
 );
 Route::post('/auth/google', function (Request $request): JsonResponse {
     $request->validate([
-        'code'=>['required','string']
+        'code' => ['required', 'string'],
+        'redirect' => ['required', 'url'],
     ]);
-    $google = Socialite::driver('google')->stateless()->user();
+    $google = Socialite::driver('google')
+        ->stateless()
+        ->redirectUrl($request->input('redirect'))
+        ->user();
 
-    return response()->json(['token'=>tap(User::query()->firstOrCreate(
+    return response()->json(['token' => tap(User::query()->firstOrCreate(
         [
             'email' => $google->getEmail(),
         ],
         ['email_verified_at' => now()]
-    ), fn (User $user) => $user->update(['name'=>$google->getName()]))->createToken('api')->plainTextToken]);
+    ), fn (User $user) => $user->update(['name' => $google->getName()]))->createToken('api')->plainTextToken]);
 })->withoutMiddleware(VerifyCsrfToken::class);
