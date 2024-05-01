1ad506214db121336597e125bea8adf0967e4b40
Marshall Davis <mdavis@teamworks.com>
2024-05-01 09:38:05 -0400

    Remove CORS from API

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
+    'paths' => ['v1/*', 'sanctum/csrf-cookie', 'auth/*'],
 
     'allowed_methods' => ['*'],
 
