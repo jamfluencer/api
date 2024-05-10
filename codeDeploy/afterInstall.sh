9e4e004c812f241da1dd43af182d23ecea10634e
Marshall Davis <toothlessrebel@gmail.com>
2024-05-10 01:28:33 -0400

    Restart stuff

--- codeDeploy/afterInstall.sh
+++ codeDeploy/afterInstall.sh
@@ -2,4 +2,5 @@
 
 cp /home/ec2-user/jamfluencer.env /usr/share/nginx/jamfluencer-api/.env
 cd /usr/share/nginx/jamfluencer-api || exit 1
+supervisorctl restart reverb queues
 su nginx -s /bin/bash -c "php artisan migrate --force"
