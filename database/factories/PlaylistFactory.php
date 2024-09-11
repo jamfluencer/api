f716800b7b9e8b43ac615e588e7997608e319a27
Marshall Davis <toothlessrebel@gmail.com>
2024-09-11 11:01:25 -0400

    Replicate Via Tests

--- database/factories/PlaylistFactory.php
+++ database/factories/PlaylistFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use App\Playback\Playlist;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Str;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<Playlist>
--- tests/Playback/AttributionTest.php
+++ tests/Playback/AttributionTest.php
@@ -134,3 +134,63 @@ describe('For Non-collaborative Playlists', function () {
             ->toBetrue('Track was attributed to incorrect occurrence.');
     });
 });
+
+it('detaches previous tracks for update', function () {
+    $replaced = PlaylistModel::factory()
+        ->hasAttached(TrackModel::factory()->count(10), fn () => ['added_by' => 'spacey_prince'])
+        ->create();
+    Spotify::shouldReceive('setToken')->once()->andReturnSelf();
+    Spotify::shouldReceive('playlist')->once()->with("{$replaced->id}", true)->andReturn(
+        new Playlist(
+            name: $this->faker->name(),
+            id: $replaced->id,
+            images: [],
+            tracks: $tracks = [
+                new Track(
+                    name: fake()->word(),
+                    album: new Album(
+                        Str::random(),
+                        $this->faker->name(),
+                        []
+                    ),
+                    artists: [],
+                    id: Str::random(),
+                    added_by: fake()->name()
+                ),
+                new Track(
+                    name: $this->faker->name(),
+                    album: new Album(
+                        Str::random(),
+                        $this->faker->name(),
+                        []
+                    ),
+                    artists: [],
+                    id: Str::random(),
+                    added_by: fake()->name()
+                ),
+                new Track(
+                    name: $this->faker->name(),
+                    album: new Album(
+                        Str::random(),
+                        $this->faker->name(),
+                        []
+                    ),
+                    artists: [],
+                    id: Str::random(),
+                    added_by: fake()->name()
+                ),
+            ],
+            totalTracks: count($tracks),
+            next: '',
+            url: $this->faker->url(),
+            snapshot: Str::random(),
+            collaborative: false
+        )
+    );
+
+    App::make(StorePlaylist::class, ['user' => User::factory()->withSpotify()->create(), 'id' => $replaced->id])->handle();
+
+    expect($replaced->tracks()->count())->toBe(3);
+})->only();
+
+it('attributes the song when reporting', function () {})->todo();
