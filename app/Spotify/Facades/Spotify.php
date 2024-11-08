71531cc30e66d54675ef639c71d382ffe13f9650
Marshall Davis <toothlessrebel@gmail.com>
2024-11-08 01:53:26 -0500

    Support Client Credentials
     For requests that do not require authorization Spotify allows
    for client credentials. Machine to machine requests for public
    data can be used in this way.

--- app/Spotify/Facades/Spotify.php
+++ app/Spotify/Facades/Spotify.php
@@ -3,10 +3,13 @@
 namespace App\Spotify\Facades;
 
 use App\Spotify\Authentication\AccessToken;
+use App\Spotify\Authentication\RefreshToken;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \App\Spotify\Spotify setToken(AccessToken $token)
+ * @method static AccessToken refreshToken(RefreshToken $token)
+ * @method static \App\Spotify\Spotify withClientCredentials()
  */
 class Spotify extends Facade
 {
