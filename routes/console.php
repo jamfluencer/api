98491e7e847b8c5cff74ba653776ae2d2b16643e
Marshall Davis <mdavis@teamworks.com>
2025-04-16 19:19:11 -0400

    Initial

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+/** @var ClosureCommand $this */
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
